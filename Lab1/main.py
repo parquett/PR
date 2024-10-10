14a56277310190f42e641a96e9a5881b3db74e1f
parquett <boss.leoneli@mail.ru>
2024-10-10 16:32:31 +0300

    complete task for grade 4

--- Lab1/main.py
+++ Lab1/main.py
@@ -14,8 +14,25 @@ else:
 # Parse the HTML content
 soup = BeautifulSoup(response.content, 'html.parser')
 
-for product in soup.find_all('div', class_='ads-list-photo-item'):
-    name = product.find('div', class_='ads-list-photo-item-title').text.strip()
-    price = product.find('div', class_='ads-list-photo-item-price').text.strip()
-    print(f"Product Name: {name}, Price: {price}")
+for product in soup.find_all('li', class_='ads-list-photo-item'):
+    link = product.find('a')
+
+    if link and 'href' in link.attrs:
+        product_url = f"https://999.md{link['href']}"
+        product_response = requests.get(product_url)
+        product_soup = BeautifulSoup(product_response.content, 'html.parser')
+
+        # Extract additional info
+        for feature in product_soup.find_all('span', class_='adPage__content__features__key'):
+            key = feature.text.strip()
+            value_tag = feature.find_next('span', class_='adPage__content__features__value')
+
+            if value_tag:
+                value = value_tag.text.strip()
+            else:
+                value = "['href'-"
+            print(f"{key}: {value}")
+
+        print(f"Product Link: {product_url}\n")
+
 
