501bd60880b7dd3f25224e1db8b5dca0a932cf48
parquett <boss.leoneli@mail.ru>
2024-10-10 16:09:41 +0300

    complete task for grade 3

--- Lab1/main.py
+++ Lab1/main.py
@@ -1,7 +1,8 @@
 import requests
+from bs4 import BeautifulSoup
 
 # Send HTTP GET request
-url = 'https://999.md/ro/list/transport/motorcycles'  # Example URL
+url = 'https://999.md/ro/list/transport/motorcycles'
 response = requests.get(url)
 
 # Check the response
@@ -9,3 +10,12 @@ if response.status_code == 200:
     print("Success:", response.status_code)
 else:
     print("Failed:", response.status_code)
+
+# Parse the HTML content
+soup = BeautifulSoup(response.content, 'html.parser')
+
+for product in soup.find_all('div', class_='ads-list-photo-item'):
+    name = product.find('div', class_='ads-list-photo-item-title').text.strip()
+    price = product.find('div', class_='ads-list-photo-item-price').text.strip()
+    print(f"Product Name: {name}, Price: {price}")
+
