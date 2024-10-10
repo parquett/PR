afbe21204f0f5e2cacccda9bbfb5b9e51b683fc0
parquett <boss.leoneli@mail.ru>
2024-10-10 17:14:19 +0300

    complete task for grade 5

--- Lab1/main.py
+++ Lab1/main.py
@@ -11,6 +11,21 @@ if response.status_code == 200:
 else:
     print("Failed:", response.status_code)
 
+
+def validate_string_field(field):
+    """ Remove extra spaces from string fields. """
+    return field.strip()
+
+
+def validate_price_field(price):
+    """ Validate price to ensure it's an integer. """
+    try:
+        # Removing any non-digit characters and converting to integer
+        return int(''.join(filter(str.isdigit, price)))
+    except ValueError:
+        return None
+
+
 # Parse the HTML content
 soup = BeautifulSoup(response.content, 'html.parser')
 
@@ -22,17 +37,34 @@ for product in soup.find_all('li', class_='ads-list-photo-item'):
         product_response = requests.get(product_url)
         product_soup = BeautifulSoup(product_response.content, 'html.parser')
 
+        # Make dictionary
+        product_data = {}
+
         # Extract additional info
         for feature in product_soup.find_all('span', class_='adPage__content__features__key'):
-            key = feature.text.strip()
+            key = validate_string_field(feature.text.strip())
             value_tag = feature.find_next('span', class_='adPage__content__features__value')
 
             if value_tag:
-                value = value_tag.text.strip()
+                value = validate_string_field(value_tag.text.strip())
             else:
-                value = "['href'-"
-            print(f"{key}: {value}")
+                value = "-"
 
+            product_data[key] = value
+
+        # Extract the price
+        price_value_tag = product_soup.find('span', class_='adPage__content__price-feature__prices__price__value')
+        currency_tag = product_soup.find('span', class_='adPage__content__price-feature__prices__price__currency')
+
+        if price_value_tag and currency_tag:
+            price_value = validate_price_field(price_value_tag.text.strip())  # Convert price to integer
+            currency_value = validate_string_field(currency_tag.text.strip())  # Extract currency
+        else:
+            price_value = "Negociabil"
+            currency_value = ""
+
+        combined_price = f"{price_value} {currency_value}"
+        product_data["Price"] = combined_price
+
+        print(f"Product Data: {product_data}")
         print(f"Product Link: {product_url}\n")
-
-
